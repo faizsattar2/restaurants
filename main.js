53e82a55aa46aa6b99c73f956822c44820980228
Faiz Sattar <jamming1251hshha@gmail.com>
2022-03-16 10:43:46 +0000

    update main.js

--- main.js
+++ main.js
@@ -1,23 +1,6 @@
-const util = require('util');
-
 const Restaurant = require('./Restaurant');
 const Menu = require('./Menu');
 const Item = require('./Item');
 
-Item.init();
-Menu.init();
-Restaurant.init();
-
-// const kfc = new Restaurant("KFC");
-// const drinks = new Menu(1, "Drinks");
-// kfc.addMenu(drinks);
-
-// const coke = new Item(1, 'Coke', 2);
-// Restaurant.all[0].menus[0].addItem(coke);
-
-console.log(util.inspect(Restaurant.all, {
-    depth: Infinity,
-    colors: true
-}));
 
 module.exports = { Item, Menu, Restaurant };
